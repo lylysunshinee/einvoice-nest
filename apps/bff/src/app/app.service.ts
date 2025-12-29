17f724bb62dbff08ad46a972c6fe1088302311b1
Nguyễn Hà Đông <hadongit66@gmail.com>
2025-12-30 00:29:54 +0700

    init husky

---
+++ apps/bff/src/app/app.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+
+// import {PORT} from "@common/constants";
+import {PORT} from "@common/constants/common.constant"
+@Injectable()
+export class AppService {
+  getData(): { message: string } {
+    console.log(PORT)
+    return { message: 'Hello API' };
+  }
+}
