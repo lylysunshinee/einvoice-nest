6e0669595dc3b942768f5bc6b2186853692d713b
Nguyễn Hà Đông <hadongit66@gmail.com>
2025-12-31 00:27:00 +0700

    chore(commit-lint): configuration lint

--- apps/bff/src/app/app.service.ts
+++ apps/bff/src/app/app.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@nestjs/common';
 
 // import {PORT} from "@common/constants";
-import {PORT} from "@common/constants/common.constant"
+import { PORT } from '@common/constants/common.constant';
 @Injectable()
 export class AppService {
   getData(): { message: string } {
-    console.log(PORT)
+    console.log(PORT);
     return { message: 'Hello API' };
   }
 }
