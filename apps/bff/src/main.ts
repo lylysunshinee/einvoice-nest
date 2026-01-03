138959bb698c3600007af07a6e29d0d48fc366be
Nguyễn Hà Đông <hadongit66@gmail.com>
2026-01-04 00:07:36 +0700

    feat/#configuration: add configuration module with environment variables and validation

--- apps/bff/src/main.ts
+++ apps/bff/src/main.ts
@@ -3,18 +3,44 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
+  const globalPrefix = AppModule.CONFIGURATION.GLOBAL_PREFIX;
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+  const port = AppModule.CONFIGURATION.APP_CONFIG.PORT;
+  // Swagger Documentation
+  const config = new DocumentBuilder()
+    .setTitle('Einvoice-bff API')
+    .setDescription('The Einvoice-bff API description')
+    .setVersion('1.0.0')
+    .addBearerAuth({
+      description: 'Default JWT Authorization',
+      type: 'http',
+      in: 'header',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+      name: 'Authorization',
+    })
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(`${globalPrefix}/docs`, app, documentFactory);
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
+  );
+  Logger.log(
+    `🚀 Swagger docs available on: http://localhost:${port}/${globalPrefix}/docs`,
   );
 }
 
---
+++ libs/configuration/src/lib/app.config.ts
@@ -0,0 +1,9 @@
+import { IsNumber } from 'class-validator';
+
+export class AppConfiguration {
+  @IsNumber()
+  PORT: number;
+  constructor() {
+    this.PORT = Number(process.env['PORT'] || 3300);
+  }
+}
